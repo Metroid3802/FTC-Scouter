ddb6dee3d95ba9d52eefc10dba4800b1f1201959
Connor McAtee <connor.mcatee@gmail.com>
2013-01-24 19:33:12 -0600

    It compiles again! Doesn't really do much though

--- FTCAllianceCalculator/src/org/cmcatee/ftcalliancecalculator/MainActivity.java
+++ FTCAllianceCalculator/src/org/cmcatee/ftcalliancecalculator/MainActivity.java
@@ -2,14 +2,21 @@ package org.cmcatee.ftcalliancecalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
+	private boolean mAlternateTitle = false;
+	
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+		
+	}
 }
