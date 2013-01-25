86043f19483bf4215b14c1350fe72efde64e644d
Connor McAtee <connor.mcatee@gmail.com>
2013-01-24 18:02:51 -0600

    updates updates updates
    I don't even remember what I updated tbh

---
+++ FTCAllianceCalculator/gen/R.java.d
@@ -0,0 +1,4 @@
+C:\Users\Connor\Documents\GitHub\FTC-Alliance-Calculator-Tallyboard\FTCAllianceCalculator\gen\org\cmcatee\ftcalliancecalculator\R.java \
+ : C:\Users\Connor\Documents\GitHub\FTC-Alliance-Calculator-Tallyboard\FTCAllianceCalculator\res\layout\main.xml \
+C:\Users\Connor\Documents\GitHub\FTC-Alliance-Calculator-Tallyboard\FTCAllianceCalculator\res\values\strings.xml \
+C:\Users\Connor\Documents\GitHub\FTC-Alliance-Calculator-Tallyboard\FTCAllianceCalculator\bin\AndroidManifest.xml \
---
+++ FTCAllianceCalculator/gen/org/cmcatee/ftcalliancecalculator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.cmcatee.ftcalliancecalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
